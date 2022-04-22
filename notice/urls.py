c36be1e59cbd8a4119888e035d23c03ecbde9adc
dnjsqls820 <dnjsqls820@naver.com>
2022-04-22 10:34:30 +0900

    urls.py 오타수정

--- notice/urls.py
+++ notice/urls.py
@@ -1,5 +1,9 @@
 # notice/urls.py
-
+# URL경로 입력으로 media 파일 접근 제한
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.urls import path
 from . import views
 
@@ -13,3 +17,10 @@ urlpatterns = [
     path('write/', views.notice_write_view, name='notice_write'),
     path('download/<int:pk>', views.notice_download_view, name = 'noitce_download'),
 ]
+
+
+#URL경로 입력media 파일 접근 제한
+def protected_file(request, path, document_root=None):
+    messages.error(request, '접근 불가')
+    return redirect('/')
+urlpatterns += static(settings.MEDIA_URL, protected_file, document_root=settings.MEDIA_ROOT)
